db1704ac8d2f830c5ac93712042b98c4cf15315f
Luthrasaab <sachinrocks.rocks9@gmail.com>
2016-09-28 11:52:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/C-language.html',function(req, res){
-   res.sendFile(path.join(__dirname,'ui','C-language.html')); 
-});
-app.get('/Java-language.html',function(req, res){
-   res.sendFile(path.join(__dirname,'ui','Java-language.html')); 
-});
-app.get('/Python-language.html',function(req, res){
-   res.sendFile(path.join(__dirname,'ui','Python-language.html')); 
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
