0889a1f0063010dadaf75df9af7e3cf22a99ffb2
Luthrasaab <sachinrocks.rocks9@gmail.com>
2016-10-01 13:43:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
-var counter = 0;
+
 var button = document.getElementById('b1');
+var counter = 0;
 button.onclick = function(){
   counter = counter + 1;  
   var span = document.getElementById('s1');
